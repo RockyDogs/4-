46143cdf66dc71c222d2b98a92ab1311e9f70fd6
RockyDogs <116272967+RockyDogs@users.noreply.github.com>
2023-03-03 16:09:10 +0500

    Create Weapons.java

---
+++ Weapons.java
@@ -0,0 +1,9 @@
+package Weapons;
+
+public abstract class Weapon implements Weaponable {
+    protected int pointOfDamage;
+
+    public Weapon(int pointOfDamage) {
+        this.pointOfDamage = pointOfDamage;
+    }
+}
