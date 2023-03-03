b0a7504545187a040caed98a550c2d8eef233229
RockyDogs <116272967+RockyDogs@users.noreply.github.com>
2023-03-03 16:14:26 +0500

    Create Shield.java

---
+++ Shield.java
@@ -0,0 +1,19 @@
+package Shields;
+
+public class Shield {
+
+    public Shield(int i, int j) {
+    }
+
+    public void addDamage(int value) {
+    }
+
+    public int getSize() {
+        return 0;
+    }
+
+    public int getArmorReserve() {
+        return 0;
+    }
+
+}
